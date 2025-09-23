69a08bb8be3549b154d7fe328001c0a8c58e9bf0
talib <talib@pcsmcpl.net>
2025-09-24 00:26:18 +0530

    version 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express.js!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
